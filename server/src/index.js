3b84f94c952b087a53e06f2d7d5c13b8da6dd153
BEN-ADDI <benaddiabderrahim1609@gmail.com>
2026-08-08 20:50:53 +0200

    feat: complete auth controllers and middleware  8/8/26

--- server/src/index.js
+++ server/src/index.js
@@ -4,15 +4,9 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const config = require("./config/env");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 const notFound = require("./middleware/notFound");
 const errorHandler = require("./middleware/errorHandler");
-
-const app = express();
-
-app.use(express.json());
-
-const authRoutes = require("./routes/authRoutes");
-// const { notFound, errorHandler } = require("./middleware/errorHandler");
 
 const app = express();
 
